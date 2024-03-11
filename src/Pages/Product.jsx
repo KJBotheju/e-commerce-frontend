de6afbde58422cc43829d85ec57164f9232a830e
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-03-11 21:01:06 +0530

    add product display page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom';
+import { Shopcontext } from '../Context/Shopcontext';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_product} = useContext(Shopcontext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=>e.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrums product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
