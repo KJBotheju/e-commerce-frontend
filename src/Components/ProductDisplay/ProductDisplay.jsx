5536401f759e2df4eb1dab907488d4a266993709
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-03-12 10:57:41 +0530

    complete cart

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
+import { Shopcontext } from '../../Context/Shopcontext'
 
 const ProductDisplay = (Props) => {
     const {product} = Props;
+    const {addToCart} = useContext(Shopcontext);
   return (
     <div className='productdisplay'>
       <div className="productdisplay-left">
@@ -51,7 +53,7 @@ const ProductDisplay = (Props) => {
                 <div>XXL</div>
             </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
         <p className='productdisplay-right-category'><span>Category :</span>Women. T-shirt, Crop Top</p>
         <p className='productdisplay-right-category'><span>Tags :</span>Modern. Latest</p>
       </div>
