aa2a6d25f9569e6f22c0d90151c4aaf3f9562759
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-03-20 11:14:12 +0530

    add state to login page

--- src/Pages/Loginsignup.jsx
+++ src/Pages/Loginsignup.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Loginsignup.css'
 
 const Loginsignup = () => {
+
+  const [state,setState] = useState("Login");
+
+  const login = async () => {
+    console.log("login fuction executed");
+  }
+
+  const signup = async () => {
+    console.log("signup fuction executed");
+  }
+
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
+          {state === "Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account? <span>Log In</span>
-        </p>
+        <button onClick={()=>{state==='Login'?login():signup()}}>Continue</button>
+        {state === "Sign Up"?<p className="loginsignup-login">
+          Already have an account? <span onClick={()=>{setState('Login')}}>Log In</span>
+        </p>:<p className="loginsignup-login">
+          Create an account? <span onClick={()=>{setState('Sign Up')}}>Sign Up</span>
+        </p>}
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, I agree to the term of use & privacy policy.</p>
